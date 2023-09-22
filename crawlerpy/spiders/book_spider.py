0c20cb6d2c85bd5d89fc434f896169b2d25bf580
milosnowcat <ramiro.alvarez@rahcode.com>
2023-09-21 19:07:05 -0600

    comments

--- crawlerpy/spiders/book_spider.py
+++ crawlerpy/spiders/book_spider.py
@@ -47,6 +47,8 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+# The `BookCrawling` class is a web crawler that extracts book titles, prices, and availability from a
+# website.
 class BookCrawling(CrawlSpider):
     name = "books"
     allowed_domains = ["toscrape.com"]
@@ -58,6 +60,13 @@ class BookCrawling(CrawlSpider):
     )
 
     def parse_item(self, response):
+        """
+        The function parses information from a web page response and yields a dictionary with the title,
+        price, and availability of an item.
+        
+        :param response: The `response` parameter is the HTTP response object returned by the web server
+        after making a request. It contains the HTML content of the web page that we are scraping
+        """
         yield {
             "title": response.css(".product_main h1::text").get(),
             "price": response.css(".price_color::text").get(),
